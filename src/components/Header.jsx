639f88a60b87e2a452a62ee2dd32c65ca2552027
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2022-01-13 15:44:30 -0300

    cahnge logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { BiWine } from "react-icons/bi";
-import { BsHouseDoor } from "react-icons/bs";
+
 
 const Header = () => {
   return (
     <header className="w-full pt-32 text-center pb-12 text-middleBlue ">
       <div className="flex justify-center w-32 mx-auto">
         <BiWine className="mx-auto text-6xl m-2" />
-        <BsHouseDoor className="mx-auto text-6xl m-2" />
+       
       </div>
-      {/* <h1 className="text-5xl font-bold  mb-4 uppercase text-lightBlue ">
-        Winehouse
-      </h1> */}
-      <img src="/logo.png" className="mx-auto w-64 md:w-96" />
-      <h2 className="nav text-4xl mb-16 mt-4 animate-pulse mx-4">
-        Cabañas en Mendoza{" "}
+      <h1 className="text-5xl font-bold  mb-4 uppercase text-lightBlue ">
+        Cabañas Mendoza
+      </h1>
+      
+      <h2 className="nav text-2xl mb-16 mt-4 animate-pulse mx-4">
+        Tu descanso merece un lugar perfecto
       </h2>
       
     </header>
