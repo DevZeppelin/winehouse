ca22865b1d3d2edb55b7411cb9fd07deabe6ebdb
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2022-01-18 17:17:02 -0300

    change logo and animate it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { BiWine } from "react-icons/bi";
-
 
 const Header = () => {
   return (
-    <header className="w-full pt-32 text-center pb-12 text-middleBlue ">
-      <div className="flex justify-center w-32 mx-auto">
-        <BiWine className="mx-auto text-6xl m-2" />
-       
+    <header className="w-full pt-16 text-center pb-12 text-middleBlue ">
+      <div className="flex justify-center mx-auto w-48 md:w-auto">
+        <img
+          src="/logo.png"
+          alt="logo winehouse cabañas mendoza"
+          className="logo-animation"
+        />
       </div>
-      <h1 className="text-5xl font-bold  mb-4 uppercase text-lightBlue ">
-        Cabañas Mendoza
-      </h1>
-      
-      <h2 className="nav text-2xl mb-16 mt-4 animate-pulse mx-4">
-        Tu descanso merece un lugar perfecto
-      </h2>
-      
     </header>
   );
 };
